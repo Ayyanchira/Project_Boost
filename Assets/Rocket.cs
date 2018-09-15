a90265843783c9a8edacc64dc324df6fcfac2d42
Akshay Ayyanchira <ayyanchira.akshay@gmail.com>
2018-09-15 15:12:38 -0400

    Added controls to the Rocket. Added Engine sound to Rocket when Thrusting. Prefabbed almost everything in the scene

---
+++ Assets/Rocket.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidbody;
+    AudioSource engineSound;
+    Boolean isEngineThrusting = false;
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        engineSound = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+        MakeEngineSound();
+	}
+
+    private void MakeEngineSound()
+    {
+        if (isEngineThrusting){
+            if (!engineSound.isPlaying){
+                engineSound.Play();
+            }
+        }else{
+            if (engineSound.isPlaying)
+            {
+                engineSound.Pause();
+            }
+        }
+    }
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space)){
+            rigidbody.AddRelativeForce(Vector3.up);
+            isEngineThrusting = true;
+        }else{
+            isEngineThrusting = false;
+        }
+
+        if (Input.GetKey(KeyCode.A)){
+            transform.Rotate(Vector3.forward);
+        }else if(Input.GetKey(KeyCode.D)){
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
