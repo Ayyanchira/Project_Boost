23537803dd6872745ab0202d039101ecf61d664d
Akshay Ayyanchira <ayyanchira.akshay@gmail.com>
2018-09-16 06:03:25 -0400

    Serialize field added. Seperated Thrust and Rotate functions Tried adding Iron man prefab but colour was always monotonous and I couldn't keep his hands down. So I deleted it.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,7 +7,8 @@ public class Rocket : MonoBehaviour {
 
     Rigidbody rigidbody;
     AudioSource engineSound;
-    Boolean isEngineThrusting = false;
+    [SerializeField] float rotationThrust = 150f;
+    [SerializeField] float liftingThrust = 10f;
 	// Use this for initialization
 	void Start () {
         rigidbody = GetComponent<Rigidbody>();
@@ -16,37 +17,72 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
-        MakeEngineSound();
+        Thrust();
+        Rotate();
+        //MakeEngineSound();
 	}
 
-    private void MakeEngineSound()
+    void OnCollisionEnter(Collision collision)
     {
-        if (isEngineThrusting){
-            if (!engineSound.isPlaying){
-                engineSound.Play();
-            }
-        }else{
-            if (engineSound.isPlaying)
-            {
-                engineSound.Pause();
-            }
+        //foreach (ContactPoint contact in collision.contacts)
+        //{
+        //    Debug.DrawRay(contact.point, contact.normal, Color.white);
+        //}
+        //if (collision.relativeVelocity.magnitude > 2)
+        //audioSource.Play();
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK"); //todo remove this line
+                break;
+
+            default:
+                print("Dead");
+                break;
         }
     }
 
-    private void ProcessInput()
-    {
-        if(Input.GetKey(KeyCode.Space)){
-            rigidbody.AddRelativeForce(Vector3.up);
-            isEngineThrusting = true;
-        }else{
-            isEngineThrusting = false;
-        }
+    //private void MakeEngineSound()
+    //{
+    //    if (isEngineThrusting){
+    //        if (!engineSound.isPlaying){
+    //            engineSound.Play();
+    //        }
+    //    }else{
+    //        if (engineSound.isPlaying)
+    //        {
+    //            engineSound.Pause();
+    //        }
+    //    }
+    //}
 
-        if (Input.GetKey(KeyCode.A)){
-            transform.Rotate(Vector3.forward);
-        }else if(Input.GetKey(KeyCode.D)){
-            transform.Rotate(-Vector3.forward);
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true; //take manual control of rotation
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThrust * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThrust * Time.deltaTime);
+        }
+        rigidbody.freezeRotation = false;
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * liftingThrust * Time.deltaTime);
+            if (!engineSound.isPlaying)
+            {
+                engineSound.Play();
+            }
+        }
+        else
+        {
+            engineSound.Stop();
         }
     }
 }
