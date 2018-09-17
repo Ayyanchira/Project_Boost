319641ae61d2ade24820ee8484209d0d0513cbcf
Akshay Ayyanchira <ayyanchira.akshay@gmail.com>
2018-09-17 04:10:11 -0400

    Added a sandbox scene to test out features. Added multiple sound. For death and for win. (Win is not working properly though) Light setting have been changed to be generated. Enum introduced to keep track of state of Rocket. Refactored the code.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,62 +1,91 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
+    //todo fix lighting bug
+
 
     Rigidbody rigidbody;
-    AudioSource engineSound;
+    AudioSource audioSource;
+
+    [SerializeField] AudioClip engineSound;
+    [SerializeField] AudioClip blastSound;
+    [SerializeField] AudioClip winSound;
     [SerializeField] float rotationThrust = 150f;
     [SerializeField] float liftingThrust = 10f;
-	// Use this for initialization
+
+    enum State {Dying,Alive,Transcending}
+    State state = State.Alive;
+
 	void Start () {
         rigidbody = GetComponent<Rigidbody>();
-        engineSound = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-        Thrust();
-        Rotate();
-        //MakeEngineSound();
+        if(state == State.Dying){return;}
+        RespondToThrustInput();
+        RespondToRotationInput();
 	}
 
     void OnCollisionEnter(Collision collision)
     {
-        //foreach (ContactPoint contact in collision.contacts)
-        //{
-        //    Debug.DrawRay(contact.point, contact.normal, Color.white);
-        //}
-        //if (collision.relativeVelocity.magnitude > 2)
-        //audioSource.Play();
+        if (state != State.Alive) { return; } //ignore collision when dead
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("OK"); //todo remove this line
                 break;
 
+            case "Finish":
+                ExecuteSuccessSequence();
+                break;
             default:
-                print("Dead");
+                ExecuteDeathSequence();
                 break;
         }
     }
 
-    //private void MakeEngineSound()
-    //{
-    //    if (isEngineThrusting){
-    //        if (!engineSound.isPlaying){
-    //            engineSound.Play();
-    //        }
-    //    }else{
-    //        if (engineSound.isPlaying)
-    //        {
-    //            engineSound.Pause();
-    //        }
-    //    }
-    //}
+    private void ExecuteSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(winSound);
+        Invoke("LoadScene", 2f);
+    }
 
-    private void Rotate()
+    private void ExecuteDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(blastSound);
+        Invoke("LoadScene", 2f);
+    }
+
+
+    private void LoadScene()
+    {
+        if(state == State.Dying){
+            SceneManager.LoadScene(0);
+        }else if(state == State.Transcending){
+            SceneManager.LoadScene(1);
+        }
+         //todo Allow for more than 2 levels
+    }
+
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Thrust();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void RespondToRotationInput()
     {
         rigidbody.freezeRotation = true; //take manual control of rotation
         if (Input.GetKey(KeyCode.A))
@@ -70,19 +99,13 @@ public class Rocket : MonoBehaviour {
         rigidbody.freezeRotation = false;
     }
 
+
     private void Thrust()
     {
-        if (Input.GetKey(KeyCode.Space))
+        rigidbody.AddRelativeForce(Vector3.up * liftingThrust * Time.deltaTime);
+        if (!audioSource.isPlaying)
         {
-            rigidbody.AddRelativeForce(Vector3.up * liftingThrust * Time.deltaTime);
-            if (!engineSound.isPlaying)
-            {
-                engineSound.Play();
-            }
-        }
-        else
-        {
-            engineSound.Stop();
+            audioSource.PlayOneShot(engineSound);
         }
     }
 }
